d7abd519873e9a1ef60a01755fe2c2b3cb944d7a
Douglas Agenor (Dougahasu) <dougahasu@gmail.com>
2018-01-11 11:29:15 -0200

    Criação da Page

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php 
 	require_once("vendor/autoload.php");
-	$app = new \Slim\Slim();
+	use \Slim\Slim;
+	use \Hcode\Page;
+
+	$app = new Slim();
 	$app->config('debug', true);
 	$app->get('/', function() {
-		$sql = new Hcode\DB\Sql();
-		$results = $sql->select("SELECT * FROM tb_controle;");
-		echo json_encode($results);
+		$page = new Page();
+		$page->setTpl("index");
 	});
 	$app->run();
 ?>
---
+++ views-cache/header.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -0,0 +1,7 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+<html lang="pt-BR">
+	<head>
+		<meta charset="utf-8">
+		<title>Tactor Store</title>
+	</head>
+	<body>
