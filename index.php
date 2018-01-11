b175272f72005d9c8fedc15eda17f1979fa9bcb3
Douglas Agenor (Dougahasu) <dougahasu@gmail.com>
2018-01-11 14:21:41 -0200

    TPL Admin

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 	require_once("vendor/autoload.php");
 	use \Slim\Slim;
 	use \Hcode\Page;
+	use \Hcode\PageAdmin;
 
 	$app = new Slim();
 	$app->config('debug', true);
@@ -9,5 +10,11 @@
 		$page = new Page();
 		$page->setTpl("index");
 	});
+
+	$app->get('/admin', function() {
+		$page = new PageAdmin();
+		$page->setTpl("index");
+	});
+
 	$app->run();
 ?>
